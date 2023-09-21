eacdb7174ae9098fa7e81f4e15f485c66f44b5bd
ERae Jo <eraejo@ERaeui-MacBookAir.local>
2023-09-21 21:10:44 +0900

    intial commit

---
+++ src/components/Board.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Square from "./Square";
+import "./Board.css";
+
+const Board = ({ squares, onClick }) => {
+  const rederSquare = (i) => {
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
+  };
+
+  return (
+    <div className="board-wrapper">
+      <div className="board-row">
+        {rederSquare(0)}
+        {rederSquare(1)}
+        {rederSquare(2)}
+      </div>
+      <div className="board-row">
+        {rederSquare(3)}
+        {rederSquare(4)}
+        {rederSquare(5)}
+      </div>
+      <div className="board-row">
+        {rederSquare(6)}
+        {rederSquare(7)}
+        {rederSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
